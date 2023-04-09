ab3b5473feea28b64e911b883f922046be1cbe2d
Vlad Kovalev <kovalevvvlad981@gmail.com>
2023-04-09 21:27:52 +0300

    delete creating Admin in main class

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,17 +1,12 @@
 package application;
 
-import application.entity.Role;
-import application.entity.Status;
 import application.entity.Topic;
-import application.entity.User;
 import application.repository.TopicRepository;
-import application.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 
@@ -22,20 +17,8 @@ public class Application {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(@Autowired UserService userService,
-                                               @Autowired TopicRepository topicRepository,
-                                               @Autowired PasswordEncoder passwordEncoder) {
+    public CommandLineRunner commandLineRunner(@Autowired TopicRepository topicRepository) {
         return args -> {
-            if (userService.findUserByLogin("ADMIN") == null) {
-                User user = new User();
-                user.setLogin("ADMIN");
-                user.setPassword(passwordEncoder.encode("ADMINadmin1"));
-                Role role = new Role();
-                role.setRoleName("ADMIN");
-                Status status = new Status();
-                status.setStatusName("ACTIVE");
-                userService.saveUser(user, role, status);
-            }
             List<Topic> topics = topicRepository.findAll();
             if (topics.size() != 3) {
                 Topic topic1 = new Topic();
